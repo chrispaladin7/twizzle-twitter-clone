9f69c6a126a41df0c7bd8fd47d486befc7b41d3a
chrispaladin7 <chrismartey@yahoo.com>
2024-03-11 15:36:31 -0400

    implements create profile

--- social/twizzle/models.py
+++ social/twizzle/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 
 # Create Profile Model.
@@ -14,3 +15,15 @@ class Profile(models.Model):
 
     def __str__(self) -> str:
         return self.user.username
+    
+#Create Profile when New User Signs Up
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user = instance)
+        user_profile.save()
+        #Have user Follow themselves
+        user_profile.follows.set([instance.profile.id])
+        user_profile.save()
+
+post_save.connect(create_profile,sender=User)
+
